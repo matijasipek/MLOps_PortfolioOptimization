e9563214d8eb63241fd4cc1515b290d4f5f934ed
Matija Šipek <mxs1943@g.rit.edu>
2024-01-11 12:01:18 +0100

    changed train_model and added data unittesting
    Tested
    - X_train.csv
    - X_test.csv
    
    Changed train_model.py to OOP class to enable easier unittesting

--- portfolio/src/train_model.py
+++ portfolio/src/train_model.py
@@ -6,57 +6,65 @@ from skfolio import RatioMeasure, RiskMeasure
 from skfolio.metrics import make_scorer
 import os
 import pandas as pd
-import pickle
-
-### INITIALIZATION OF MODELS
-models = Models()
-model_stacking = models.give_model_stacking()
-benchmark = models.give_benchmark()
-
-### PARAMETER TUNING
-cv = WalkForward(train_size=252, test_size=60)
-grid_search = GridSearchCV(
-    estimator=model_stacking,
-    cv=cv,
-    n_jobs=-1,
-    param_grid={
-        "model2__prior_estimator__covariance_estimator": [
-            EmpiricalCovariance(),
-            LedoitWolf(),
-        ],
-        "model3__l1_coef": [0.001, 0.1],
-        "model4__risk_measure": [
-            RiskMeasure.VARIANCE,
-            RiskMeasure.GINI_MEAN_DIFFERENCE,
-        ],
-    },
-    scoring=make_scorer(RatioMeasure.CALMAR_RATIO),
-)
-
-### TRAINING
-# DATA
-script_dir = os.path.dirname(__file__)  # get the directory of the current script
-processed_data_dir = os.path.join(script_dir, '../data/processed')
-
-X_train = pd.read_csv(os.path.join(processed_data_dir, 'X_train.csv'), index_col=0)
-# fit the model
-grid_search.fit(X_train)
-model_stacking = grid_search.best_estimator_
-
-
-### SAVE MODEL ###  
 import dill as pickle
-script_dir = os.path.dirname(__file__)  # get the directory of the current script
-model_dir = os.path.join(script_dir, '../models/')# Define the path to save the model file
-model_path = os.path.join(model_dir, 'model.pkl')
-benchmark_path = os.path.join(model_dir, 'benchmark.pkl')
 
-# MODEL
-with open(model_path, 'wb') as file:
-    pickle.dump(model_stacking, file)
-print(f"Model saved to {model_path}")
-# BENCHMARK
-with open(benchmark_path, 'wb') as file:
-    pickle.dump(benchmark, file)
-print(f"Benchmark saved to {benchmark_path}")
 
+class ModelTrainer:
+    def __init__(self):
+        self.models = Models()
+        self.model_stacking = self.models.give_model_stacking()
+        self.benchmark = self.models.give_benchmark()
+
+    def tune_parameters(self):
+        self.cv = WalkForward(train_size=252, test_size=60)
+        self.grid_search = GridSearchCV(
+            estimator=self.model_stacking,
+            cv=self.cv,
+            n_jobs=-1,
+            param_grid={
+                "model2__prior_estimator__covariance_estimator": [
+                    EmpiricalCovariance(),
+                    LedoitWolf(),
+                ],
+                "model3__l1_coef": [0.001, 0.1],
+                "model4__risk_measure": [
+                    RiskMeasure.VARIANCE,
+                    RiskMeasure.GINI_MEAN_DIFFERENCE,
+                ],
+            },
+            scoring=make_scorer(RatioMeasure.CALMAR_RATIO),
+        )
+
+    def train(self, X_train):
+        self.grid_search.fit(X_train)
+        self.model_stacking = self.grid_search.best_estimator_
+
+    def save_model(self, model_path, benchmark_path):
+        with open(model_path, 'wb') as file:
+            pickle.dump(self.model_stacking, file)
+        with open(benchmark_path, 'wb') as file:
+            pickle.dump(self.benchmark, file)
+
+def main():
+    # Initialize the model trainer
+    trainer = ModelTrainer()
+
+    # Tune the parameters
+    trainer.tune_parameters()
+
+    # Load the training data
+    script_dir = os.path.dirname(__file__)
+    processed_data_dir = os.path.join(script_dir, '../data/processed')
+    X_train = pd.read_csv(os.path.join(processed_data_dir, 'X_train.csv'), index_col=0)
+
+    # Train the model
+    trainer.train(X_train)
+
+    # Save the model
+    model_dir = os.path.join(script_dir, '../models/')
+    model_path = os.path.join(model_dir, 'model.pkl')
+    benchmark_path = os.path.join(model_dir, 'benchmark.pkl')
+    trainer.save_model(model_path, benchmark_path)
+
+if __name__ == "__main__":
+    main()
---
+++ portfolio/tests/test_dataset.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import os
+from tests import _PATH_DATA
+
+def test_dataframe_type():
+    dataset = pd.read_csv(os.path.join(_PATH_DATA+'/processed/', "X_train.csv"))
+    assert isinstance(dataset, pd.DataFrame)
+
+def test_non_empty():
+    dataset = pd.read_csv(os.path.join(_PATH_DATA+'/processed/', "X_train.csv"))
+    assert not dataset.empty
+
+def test_columns():
+    dataset = pd.read_csv(os.path.join(_PATH_DATA+'/processed/', "X_train.csv"))
+    expected_columns = ['Date', 'AAL.L', 'ABF.L', 'AHT.L', 'ANTO.L', 'AV.L', 'AZN.L', 'BA.L',
+       'BARC.L', 'BATS.L', 'BDEV.L', 'BKG.L', 'BLND.L', 'BNZL.L', 'BP.L',
+       'BT-A.L', 'CNA.L', 'CRDA.L', 'DGE.L', 'FCIT.L', 'GSK.L', 'HLMA.L',
+       'HSBA.L', 'HSX.L', 'III.L', 'IMB.L', 'INF.L', 'JD.L', 'JMAT.L', 'KGF.L',
+       'LAND.L', 'LGEN.L', 'LLOY.L', 'NG.L', 'NWG.L', 'NXT.L', 'PRU.L',
+       'PSN.L', 'PSON.L', 'REL.L', 'RIO.L', 'RKT.L', 'RR.L', 'RTO.L', 'SBRY.L',
+       'SDR.L', 'SGE.L', 'SGRO.L', 'SMDS.L', 'SMIN.L', 'SMT.L', 'SN.L',
+       'SPX.L', 'SSE.L', 'STAN.L', 'STJ.L', 'SVT.L', 'TSCO.L', 'TW.L',
+       'ULVR.L', 'UU.L', 'VOD.L', 'WEIR.L', 'WPP.L', 'WTB.L'] 
+    assert list(dataset.columns) == expected_columns
+
+def test_no_nan_values():
+    dataset = pd.read_csv(os.path.join(_PATH_DATA+'/processed/', "X_train.csv"))
+    assert not dataset.isnull().values.any()
+
+def test_dataframe_type_test():
+    dataset = pd.read_csv(os.path.join(_PATH_DATA+'/processed/', "X_test.csv"))
+    assert isinstance(dataset, pd.DataFrame)
+
+def test_non_empty_test():
+    dataset = pd.read_csv(os.path.join(_PATH_DATA+'/processed/', "X_test.csv"))
+    assert not dataset.empty
+
+def test_columns_test():
+    dataset = pd.read_csv(os.path.join(_PATH_DATA+'/processed/', "X_test.csv"))
+    expected_columns = ['Date', 'AAL.L', 'ABF.L', 'AHT.L', 'ANTO.L', 'AV.L', 'AZN.L', 'BA.L',
+       'BARC.L', 'BATS.L', 'BDEV.L', 'BKG.L', 'BLND.L', 'BNZL.L', 'BP.L',
+       'BT-A.L', 'CNA.L', 'CRDA.L', 'DGE.L', 'FCIT.L', 'GSK.L', 'HLMA.L',
+       'HSBA.L', 'HSX.L', 'III.L', 'IMB.L', 'INF.L', 'JD.L', 'JMAT.L', 'KGF.L',
+       'LAND.L', 'LGEN.L', 'LLOY.L', 'NG.L', 'NWG.L', 'NXT.L', 'PRU.L',
+       'PSN.L', 'PSON.L', 'REL.L', 'RIO.L', 'RKT.L', 'RR.L', 'RTO.L', 'SBRY.L',
+       'SDR.L', 'SGE.L', 'SGRO.L', 'SMDS.L', 'SMIN.L', 'SMT.L', 'SN.L',
+       'SPX.L', 'SSE.L', 'STAN.L', 'STJ.L', 'SVT.L', 'TSCO.L', 'TW.L',
+       'ULVR.L', 'UU.L', 'VOD.L', 'WEIR.L', 'WPP.L', 'WTB.L'] 
+    assert list(dataset.columns) == expected_columns
+
+def test_no_nan_values_test():
+    dataset = pd.read_csv(os.path.join(_PATH_DATA+'/processed/', "X_test.csv"))
+    assert not dataset.isnull().values.any()
